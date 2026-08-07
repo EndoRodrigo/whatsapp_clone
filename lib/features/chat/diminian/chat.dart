4cb3c6e4c5af54a164e33ace5162488921543336
EndoRodrigo <endorodrigo19@gmail.com>
2026-08-06 20:10:03 -0500

    fix: actualizacion de la clase chat

--- lib/features/chat/diminian/chat.dart
+++ lib/features/chat/diminian/chat.dart
@@ -1,8 +1,57 @@
 class Chat {
+  final int id;
   final String name;
   final String lastMessage;
   final String hour;
-  bool leido;
+  final bool isRead;
+  final bool isFavorite;
 
-  Chat({required this.name, required this.lastMessage, required this.hour, required this.leido});
+  const Chat({
+    required this.id,
+    required this.name,
+    required this.lastMessage,
+    required this.hour,
+    required this.isRead,
+    required this.isFavorite,
+  });
+
+  Chat copyWith({
+    int? id,
+    String? name,
+    String? lastMessage,
+    String? hour,
+    bool? isRead,
+    bool? isFavorite,
+  }) {
+    return Chat(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      lastMessage: lastMessage ?? this.lastMessage,
+      hour: hour ?? this.hour,
+      isRead: isRead ?? this.isRead,
+      isFavorite: isFavorite ?? this.isFavorite,
+    );
+  }
+
+  factory Chat.fromMap(Map<String, dynamic> map) {
+    return Chat(
+      id: map['id'],
+      name: map['name'],
+      lastMessage: map['lastMessage'],
+      hour: map['hour'],
+      isRead: map['isRead'] == 1,
+      isFavorite: map['isFavorite'] == 1,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'lastMessage': lastMessage,
+      'hour': hour,
+      'isRead': isRead ? 1 : 0,
+      'isFavorite': isFavorite ? 1 : 0,
+    };
+  }
 }
