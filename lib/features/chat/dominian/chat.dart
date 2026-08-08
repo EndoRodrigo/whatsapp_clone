d321af6aa65458703bbd84d299101520a6c57e7c
EndoRodrigo <endorodrigo19@gmail.com>
2026-08-08 11:48:22 -0500

    fix: se modifica la clase chat

--- lib/features/chat/dominian/chat.dart
+++ lib/features/chat/dominian/chat.dart
@@ -3,16 +3,18 @@ class Chat {
   final String name;
   final String lastMessage;
   final String hour;
-  final bool isRead;
   final bool isFavorite;
+  final bool isRead;
+  final String? photoUrl;
 
-  const Chat({
+  Chat({
     required this.id,
     required this.name,
     required this.lastMessage,
     required this.hour,
-    required this.isRead,
-    required this.isFavorite,
+    this.isFavorite = false,
+    this.isRead = false,
+    this.photoUrl,
   });
 
   Chat copyWith({
@@ -20,27 +22,30 @@ class Chat {
     String? name,
     String? lastMessage,
     String? hour,
-    bool? isRead,
     bool? isFavorite,
+    bool? isRead,
+    String? photoUrl,
   }) {
     return Chat(
       id: id ?? this.id,
       name: name ?? this.name,
       lastMessage: lastMessage ?? this.lastMessage,
       hour: hour ?? this.hour,
-      isRead: isRead ?? this.isRead,
       isFavorite: isFavorite ?? this.isFavorite,
+      isRead: isRead ?? this.isRead,
+      photoUrl: photoUrl ?? this.photoUrl,
     );
   }
 
   factory Chat.fromMap(Map<String, dynamic> map) {
     return Chat(
-      id: map['id'],
-      name: map['name'],
-      lastMessage: map['lastMessage'],
-      hour: map['hour'],
-      isRead: map['isRead'] == 1,
+      id: map['id'] as int,
+      name: map['name'] as String,
+      lastMessage: map['lastMessage'] as String,
+      hour: map['hour'] as String,
       isFavorite: map['isFavorite'] == 1,
+      isRead: map['isRead'] == 1,
+      photoUrl: map['photoUrl'] as String?,
     );
   }
 
@@ -50,8 +55,9 @@ class Chat {
       'name': name,
       'lastMessage': lastMessage,
       'hour': hour,
-      'isRead': isRead ? 1 : 0,
       'isFavorite': isFavorite ? 1 : 0,
+      'isRead': isRead ? 1 : 0,
+      'photoUrl': photoUrl,
     };
   }
 }
