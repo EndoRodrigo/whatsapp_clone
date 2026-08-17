7a9683cda4b7eb683c01a14d22de30baac21d6af
EndoRodrigo <endorodrigo19@gmail.com>
2026-08-16 19:29:09 -0500

    fix: ae justa la clase ChatException para que el desarrollador ttenga la causa tecnica

--- lib/features/chat/dominian/exceptions/chat_exception.dart
+++ lib/features/chat/dominian/exceptions/chat_exception.dart
@@ -1,13 +1,16 @@
 class ChatException implements Exception {
   final ChatError error;
+  final Object? cause;
+  final StackTrace? stackTrace;
 
-  ChatException(this.error);
+  ChatException(this.error, {this.cause, this.stackTrace});
 }
 
 enum ChatError {
   update,
   notFound,
   network,
+  unknown
 }
 
 //Definicionde los mensajes de error
@@ -22,6 +25,9 @@ extension ChatErrorMessage on ChatError {
 
       case ChatError.network:
         return 'No hay conexión a Internet.';
+
+      case ChatError.unknown:
+        return 'Ocurrió un error inesperado. Inténtalo nuevamente.';
     }
   }
 }
